c7dc5dea60c6ac93c9fdb771cf6421e8ecf74a16
mrhorin <u0813mb@gmail.com>
2022-04-02 09:53:55 +0900

    Subjectが対応した言語かを判別できるように

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -6,4 +6,11 @@ class Subject < ApplicationRecord
   def lang
     Utils::Language.lang game.lang
   end
+
+  def supported_lang?
+    keyboards = lang[:keyboards].map do |kbd|
+      kbd if word =~ kbd[:regexp]
+    end.compact
+    keyboards.size == 1
+  end
 end
--- app/validators/subject_word_validator.rb
+++ app/validators/subject_word_validator.rb
@@ -1,13 +1,9 @@
 class SubjectWordValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
-    keyboards = record.lang[:keyboards].map do |kbd|
-      kbd if value =~ kbd[:regexp]
-    end.compact
-
     if value.nil? || value.empty?
       record.errors.add(attribute, "#{attribute} column is required.")
-    elsif keyboards.empty?
+    elsif !record.supported_lang?
       record.errors.add(attribute, "#{value} doesn't match any keyboards in #{record.game.lang}.")
     elsif value.length != record.game.char_count
       record.errors.add(attribute, "#{value} must be #{record.game.char_count} letter.")
