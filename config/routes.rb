78b92539d07374f4aece62126cc48f82114d121c
mrhorin <u0813mb@gmail.com>
2022-08-03 15:28:00 +0900

    Gmaes#create, Words#createのパスを変更

--- config/routes.rb
+++ config/routes.rb
@@ -18,20 +18,23 @@ Rails.application.routes.draw do
       scope :games do
         get 'list_current_games', to: 'games#list_current_games'
         get 'supported_langs', to: 'games#supported_langs'
+
         get ':id/word_list', to: 'games#word_list'
         get ':id/words', to: 'games#words'
+
         get ':id', to: 'games#show'
         put ':id', to: 'games#update'
         delete ':id', to: 'games#destroy'
-        post 'create', to: 'games#create'
+        post '/', to: 'games#create'
       end
 
       # WordsController
       scope :words do
         get 'today/:game_id', to: 'words#today'
+
         put ':id', to: 'words#update'
         delete ':id', to: 'words#destroy'
-        post 'create', to: 'words#create'
+        post '/', to: 'words#create'
       end
     end
   end
