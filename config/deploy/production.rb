398e8a6a62678ae33863784a1d34ffb81ac5cf23
mrhorin <u0813mb@gmail.com>
2022-09-27 01:17:14 +0900

    deploy時にproduction環境のcredentialファイルとキーをアップロードするように

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -66,3 +66,18 @@ role :db,  %w{mrhorin@sakura}
 set :default_env, {
   LANG: 'C.UTF-8'
 }
+
+# Upload production.key
+append :linked_files, "config/credentials/production.key"
+
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_master_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/credentials/production.key ]")
+          upload! 'config/credentials/production.key', "#{shared_path}/config/credentials/production.key"
+        end
+      end
+    end
+  end
+end
