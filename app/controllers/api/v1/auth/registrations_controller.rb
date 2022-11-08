fb5803c9bda92b739cafacb616b5e971e6c872d8
mrhorin <u0813mb@gmail.com>
2022-11-08 22:38:32 +0900

    変更: Registrations#destroy, update, edit で凍結時に code 1001 を返すように変更

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,9 +1,16 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  include Render
   skip_before_action :verify_authenticity_token, if: :devise_controller?
+  before_action :prevent_cookie_overflow, except: [:cancel, :new, :create, :update, :edit, ]
+  before_action :check_suspended, except: [:cancel, :new, :create]
 
-  def destroy
-    # Prevent ActionDispatch::Cookies::CookieOverflow
-    session.delete("warden.user.user.key")
-    super
-  end
+  private
+    def prevent_cookie_overflow
+      # Prevent ActionDispatch::Cookies::CookieOverflow
+      session.delete("warden.user.user.key")
+    end
+
+    def check_suspended
+      return render_current_user_suspended if @resource.is_suspended
+    end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,46 +1,7 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  include Render
   before_action :skip_session
-
-  private
-    # Users
-    def render_current_user_suspended
-      render json: { ok: false, httpStatus: 403, code: 1001, message: I18n.t('users.is_suspended')}, status: 403
-    end
-
-    def render_user_not_logged_in
-      render json: { ok: false, httpStatus: 401, code: 1002, message: I18n.t('users.not_logged_in')}, status: 401
-    end
-
-    # Games
-    def render_game_not_found
-      render json: { ok: false, httpStatus: 404, code: 2001, message: I18n.t('games.not_found')}, status: 404
-    end
-
-    def render_game_no_words
-      render json: { ok: false, httpStatus: 404, code: 2002, message: I18n.t('games.has_no_words') }, status: 404
-    end
-
-    def render_game_not_published
-      render json: { ok: false, httpStatus: 403, code: 2003, message: I18n.t('games.not_published') }, status: 403
-    end
-
-    def render_game_suspended
-      render json: { ok: false, httpStatus: 403, code: 2004, message: I18n.t('games.is_suspended')}, status: 403
-    end
-
-    def render_game_owner_suspended
-      render json: { ok: false, httpStatus: 403, code: 2005, message: I18n.t('games.owner_is_suspended')}, status: 403
-    end
-
-    def render_game_not_owner
-      render json: { ok: false, httpStatus: 401, code: 2006, message: I18n.t('games.not_owner') }, status: 401
-    end
-
-    # Word
-    def render_word_not_found
-      render json: { ok: false, httpStatus: 404, code: 3001, message: I18n.t('words.not_found')}, status: 404
-    end
 
   protected
     def skip_session
